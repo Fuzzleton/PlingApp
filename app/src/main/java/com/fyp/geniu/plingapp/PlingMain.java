24f0cd25cf129ad39106fbe6929f5dad1444d0f1
Kieran O' Mahony <112422698@umail.ucc.ie>
2016-02-06 20:04:34 +0000

    Still fiddling with event listeners pre-test

--- app/src/main/java/com/fyp/geniu/plingapp/PlingMain.java
+++ app/src/main/java/com/fyp/geniu/plingapp/PlingMain.java
@@ -1,46 +1,122 @@
 package com.fyp.geniu.plingapp;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class PlingMain extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+
+    GoogleMap mMap;
+    Marker marker;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pling_main);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+        SupportMapFragment mapFrag = (SupportMapFragment)
+                getSupportFragmentManager().
+                        findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
 
+        mMap = mapFrag.getMap();
+
+        if (mMap != null) {
+
+            mMap.setOnMapLongClickListener(new
+                                                   GoogleMap.OnMapLongClickListener() {
+                                                       @Override
+                                                       public void onMapLongClick(LatLng latLng) {
+                                                           Geocoder geocoder =
+                                                                   new Geocoder(PlingMain.this);
+                                                           List<Address> list;
+                                                           try {
+                                                               list = geocoder.getFromLocation(latLng.latitude,
+                                                                       latLng.longitude, 1);
+                                                           } catch (IOException e) {
+                                                               return;
+                                                           }
+                                                           Address address = list.get(0);
+                                                           if (marker != null) {
+                                                               marker.remove();
+                                                           }
+
+                                                           MarkerOptions options = new MarkerOptions()
+                                                                   .title(address.getLocality())
+                                                                   .position(new LatLng(latLng.latitude,
+                                                                           latLng.longitude));
+
+                                                           marker = mMap.addMarker(options);
+                                                       }
+                                                   });
+        }
+    }
 
     /**
      * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * This is where we can add markers or lines, add listeners or move the camera.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    private void onHostClick() {
+
+        Button btnHost = (Button) findViewById(R.id.btnHost);
+        btnHost.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+
+                HostEvent();
+            }
+        });
+    }
+
+    private void onViewClick() {
+
+        Button btnView = (Button) findViewById(R.id.btnView);
+        btnView.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+
+                ViewEvent();
+            }
+        });
+    }
+
+    private void HostEvent() {
+
+        Toast.makeText(PlingMain.this, "Event host method called, but not yet written.", Toast.LENGTH_LONG).show();
+
+        // Add a new intent to open a HostEvent class type doodad
+
+    }
+
+    private void ViewEvent() {
+
+        Toast.makeText(PlingMain.this, "Event view method called, but not yet written.", Toast.LENGTH_LONG).show();
+
+        // Add a new intent to open a ViewEvent class type doodad
+
     }
 }
